d5ebcfe53e25150bed7289d28ee65103d0de317b
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-16 20:07:38 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace CheckTemperatures__ClosestTo100___CheckFor5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtTemp1 = new System.Windows.Forms.TextBox();
+            this.txtTemp2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblResultCheckFor5 = new System.Windows.Forms.Label();
+            this.lblResultClosestTo100 = new System.Windows.Forms.Label();
+            this.lblResultTemperatures = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(187, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(555, 405);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(895, 404);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 33);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtTemp1
+            // 
+            this.txtTemp1.Location = new System.Drawing.Point(234, 77);
+            this.txtTemp1.Name = "txtTemp1";
+            this.txtTemp1.Size = new System.Drawing.Size(100, 26);
+            this.txtTemp1.TabIndex = 3;
+            // 
+            // txtTemp2
+            // 
+            this.txtTemp2.Location = new System.Drawing.Point(234, 141);
+            this.txtTemp2.Name = "txtTemp2";
+            this.txtTemp2.Size = new System.Drawing.Size(100, 26);
+            this.txtTemp2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter First Temparature:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter Second Temparature:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(559, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Enter First Number:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(728, 80);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 26);
+            this.txtNum1.TabIndex = 8;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(728, 137);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 26);
+            this.txtNum2.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(535, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Enter Second Number:";
+            // 
+            // lblResultCheckFor5
+            // 
+            this.lblResultCheckFor5.AutoSize = true;
+            this.lblResultCheckFor5.Location = new System.Drawing.Point(891, 322);
+            this.lblResultCheckFor5.Name = "lblResultCheckFor5";
+            this.lblResultCheckFor5.Size = new System.Drawing.Size(51, 20);
+            this.lblResultCheckFor5.TabIndex = 11;
+            this.lblResultCheckFor5.Text = "label5";
+            // 
+            // lblResultClosestTo100
+            // 
+            this.lblResultClosestTo100.AutoSize = true;
+            this.lblResultClosestTo100.Location = new System.Drawing.Point(569, 322);
+            this.lblResultClosestTo100.Name = "lblResultClosestTo100";
+            this.lblResultClosestTo100.Size = new System.Drawing.Size(51, 20);
+            this.lblResultClosestTo100.TabIndex = 12;
+            this.lblResultClosestTo100.Text = "label6";
+            // 
+            // lblResultTemperatures
+            // 
+            this.lblResultTemperatures.AutoSize = true;
+            this.lblResultTemperatures.Location = new System.Drawing.Point(198, 322);
+            this.lblResultTemperatures.Name = "lblResultTemperatures";
+            this.lblResultTemperatures.Size = new System.Drawing.Size(51, 20);
+            this.lblResultTemperatures.TabIndex = 13;
+            this.lblResultTemperatures.Text = "label7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1181, 524);
+            this.Controls.Add(this.lblResultTemperatures);
+            this.Controls.Add(this.lblResultClosestTo100);
+            this.Controls.Add(this.lblResultCheckFor5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTemp2);
+            this.Controls.Add(this.txtTemp1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Temperature And Numbers App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtTemp1;
+        private System.Windows.Forms.TextBox txtTemp2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblResultCheckFor5;
+        private System.Windows.Forms.Label lblResultClosestTo100;
+        private System.Windows.Forms.Label lblResultTemperatures;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckTemperatures__ClosestTo100___CheckFor5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Disable other buttons
+            button2.Enabled = false;
+            button3.Enabled = false;
+
+            int temp1 = int.Parse(txtTemp1.Text);
+            int temp2 = int.Parse(txtTemp2.Text);
+            bool result = CheckTemperatures(temp1, temp2);
+            lblResultTemperatures.Text = "Result: " + result;
+        }
+
+        private bool CheckTemperatures(int temp1, int temp2)
+        {
+            return temp1 < 0 && temp2 > 100;
+        }
+    
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            int result = ClosestTo100(num1, num2);
+            lblResultClosestTo100.Text = "Result: " + result;
+        }
+
+        private int ClosestTo100(int num1, int num2)
+        {
+            // Disable other buttons
+            button1.Enabled = false;
+
+            int diff1 = Math.Abs(100 - num1);
+            int diff2 = Math.Abs(100 - num2);
+
+            if (diff1 == diff2)  
+                return 0;
+            else if (diff1 < diff2)
+                return num1;
+            else
+                return num2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Disable other buttons
+            button1.Enabled = false;
+            
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            bool result = CheckFor5(num1, num2);
+            lblResultCheckFor5.Text = "Result: " + (result ? "True" : "False");
+        }
+
+        private bool CheckFor5(int num1, int num2)
+        {
+            return num1 == 5 || num2 == 5 || num1 + num2 == 5 || Math.Abs(num1 - num2) == 5;
+        }
+    }
+}
